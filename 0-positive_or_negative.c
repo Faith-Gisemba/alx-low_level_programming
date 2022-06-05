fcc96945e843d3ce2e2509ad5724613bec8c2483
Faith-Gisemba <faithmgisemba@gmail.com>
2022-06-05 09:40:17 -0700

    my commit

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,18 +1,19 @@
+#!/bin/bash
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main-prints if number is positive, zero or negative
+ * main-
  *
- * Return: Always (success)
+ * return:
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() -RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -21,9 +22,12 @@ int main(void)
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} else
-	{
-		printf('%d is positive\n", n);
 	}
-	return (0);
+	else 
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0)
 }
+
