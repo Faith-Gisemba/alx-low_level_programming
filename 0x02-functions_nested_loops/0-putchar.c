bc7afb3bf8f9b296e9026677bed0e53d2bf4b4bd
Faith-Gisemba <faithmgisemba@gmail.com>
2022-06-08 05:27:07 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "0-putchar"
 
 /**
  * main - Entry point
@@ -8,14 +7,15 @@
  */
 int main(void)
 {
+	_putchar('_');	
 	_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 		return (0);
 }
-
