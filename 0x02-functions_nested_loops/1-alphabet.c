3c0262940e21a8f7d038a147648d4a855f005e5a
Faith-Gisemba <faithmgisemba@gmail.com>
2022-06-08 12:29:53 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * ptintf_alphabet _x10-fuction that prints alphabets ten times
+ *
+ * Return; Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++;){
+		_putchar(j);
+	}
+
+	_putchar('\n');
+
+}
+print_alphabet_x10();
