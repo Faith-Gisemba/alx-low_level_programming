9ec3e12517f539edb6e04288dc5d2eb75e0c735b
Faith-Gisemba <faithmgisemba@gmail.com>
2022-06-08 12:45:26 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,18 @@
  * Return; Always 0.
  */
 
-void print_alphabet_x10(void) {
-
+void print_alphabet_x10(void)
+{
 	char j;
 
-	for (j = 'a'; j <= 'z'; j++;){
+	for (j = 'a'; j <= 'z'; j++;) 
+	{
 		_putchar(j);
 	}
 
 	_putchar('\n');
+
+	return (0);
 
 }
 print_alphabet_x10();
