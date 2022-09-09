76131ca6259d7fb7b2aab5afc830c9bf7d58ba66
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-09 14:20:26 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,15 @@
 */
 int main(void)
 {
-		char n
+	char;
 
-		for (n = '0'; n <= '9'; n++)
-		{
-			putchar(n);
-		}
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
 
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
+
