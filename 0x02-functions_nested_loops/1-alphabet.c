b21b25e52257583b644873f300b7249d1efb3b9f
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-13 06:24:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "putcher.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char x = 'y';
+
+	while (x <= 'z')
+	{
+		_putchar (x);
+		x++
+
+	}
+	_putchar ('\n');
+
+}
