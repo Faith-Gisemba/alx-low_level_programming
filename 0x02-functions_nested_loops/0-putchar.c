3a4630ea73143695f1a71a99565e89213f615dfe
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-13 12:20:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints Putchar
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('P');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
