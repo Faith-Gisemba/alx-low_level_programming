64e471d8fbd60750a00a6c12d5e7b7d01a282682
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-14 09:10:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/_alphabet.c
@@ -0,0 +1,12 @@
+#include <unistd.h>                                                                                                              
+                                                                                                                                 
+/**                                                                                                                              
+*                                                                                                                               
+* _alphabet - Write the char c to stdout                                                                                         
+*                                                                                                                               
+* Return: Always 0                                                                                                              
+*/
+void print_alphabet(void);
+{
+return (write(1, &c, 1));                                                                                                
+}
