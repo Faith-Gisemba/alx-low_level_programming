22fbb3506112e9050674456b93744fec8d984dce
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-14 15:38:57 -0700

    3-islower.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar - Write the char c to stdout
- * @c: The character to the print
- *
- * Return: on success 1
- * On error, -1 is returned,and error is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _islower(int c);
