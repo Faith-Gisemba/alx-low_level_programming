037b1e6fac51f1f41c54dade1f0129a19d78bf28
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-14 03:25:10 -0700

    0-putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: The number to be treated
  *
  * Return: Value of the last digit of number
  */
