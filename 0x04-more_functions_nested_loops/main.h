4c7f204fa973295faafe4b2d10bd24d8494b4ee3
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-15 23:54:03 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,17 +6,16 @@
  * void prototypes (void)
  * int prototypes (int)
  */
-
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
-void print most_numbers(void);
-void more_numbers(void);
+void ptint most_numbers(void);
+void print more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int _putchar(char);
-int _isupper(int c);
-int mul(int a, int b);
-
-#endif /* MAIN_H */
+#endif
