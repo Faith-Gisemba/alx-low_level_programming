39dc114e09fb5211a56e64f2629d769237799298
Faith-Gisemba <faithmgisemba@gmail.com>
2022-09-20 14:50:47 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,9 +19,7 @@ void puts_half(char *str)
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
+
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
